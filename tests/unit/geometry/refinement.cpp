7473971f470a0e890df06551a055758987702859
Andreas Longva <andreas.b.longva@gmail.com>
2016-09-21 10:09:46 +0200

    Add real world test case for refine_to_tolerance
    Currently only tests for a single tolerance. Next step
    is to make the test a parametrized test and test
    for multiple tolerance values.

--- tests/unit/geometry/refinement.cpp
+++ tests/unit/geometry/refinement.cpp
@@ -3,6 +3,8 @@
 #include <gmock/gmock.h>
 
 #include <util/vertex_matchers.hpp>
+#include <cmath>
+#include <algorithm>
 
 using Vertex = crest::Vertex<double>;
 using Element = crest::Element<unsigned int>;
@@ -10,6 +12,26 @@ using Element = crest::Element<unsigned int>;
 using ::testing::Pointwise;
 using ::testing::ElementsAreArray;
 using ::testing::Eq;
+using ::testing::AnyOf;
+using ::testing::DoubleEq;
+using ::testing::Each;
+
+template <typename T, typename I>
+std::vector<T> compute_diameters(const crest::IndexedMesh<T, I> & mesh)
+{
+    std::vector<T> diam;
+    diam.reserve(mesh.num_elements());
+    for (size_t i = 0; i < mesh.num_elements(); ++i)
+    {
+        const auto triangle = mesh.triangle_for(static_cast<I>(i));
+        const auto diameter = crest::diameter(triangle);
+        diam.push_back(diameter);
+    }
+    return diam;
+};
+
+template <typename Integer>
+bool is_even(Integer i) { return i % 2 == 0; }
 
 class refinement_test : public ::testing::Test
 {
@@ -66,4 +88,34 @@ TEST_F(refinement_test, expected_single_bisection)
 
     EXPECT_THAT(mesh.vertices(), Pointwise(VertexDoubleEq(), expected_vertices));
     EXPECT_THAT(mesh.elements(), Pointwise(Eq(), expected_elements));
+}
+
+TEST_F(refinement_test, all_elements_reasonably_sized)
+{
+    double tolerance =  0.7;
+
+    // The `unit_square_2_elements` mesh has the following useful property:
+    // The number of bisections n necessary for all edges in the mesh to have diameters smaller than tolerance
+    // is bounded by:
+    //      n >= 1 - 2 log2(tolerance).
+    // Furthermore, the length l of each edge is bounded by
+    //
+    // sqrt(2) / 2^((n + 1)/2) <= l <= 1 / 2^((n - 1)/2)    if n is odd
+    //
+    //           1 / 2^(n / 2) <= l <= sqrt(2) / 2^(n / 2)  if n is even
+    //
+    // Moreover, since any triangle in the mesh family consists of exactly one "diagonal" edge and two "straight"
+    // edges, the diameter of any element is given by the upper bound.
+
+    const unsigned int n = static_cast<unsigned int>(std::max(0.0, ceil(1 - 2.0 * std::log2(tolerance))));
+    const double upper_bound = is_even(n)
+                               ? sqrt(2.0) / pow(2.0, n / 2.0)
+                               : 1.0 / pow(2.0, (n - 1.0) / 2.0);
+    const double expected_diameter = upper_bound;
+
+    const auto mesh = crest::refine_to_tolerance(unit_square_2_elements, tolerance);
+    const auto diameters = compute_diameters(mesh);
+
+    EXPECT_THAT(diameters.size(), mesh.num_elements());
+    EXPECT_THAT(diameters, Each(DoubleEq(expected_diameter)));
 }
