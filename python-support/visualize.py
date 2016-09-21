a25f7c9726e0bfb10f78025b1d8e76c7f73bdc6e
Andreas Longva <andreas.b.longva@gmail.com>
2016-09-21 17:06:03 +0200

    Add basic python helper functionality

---
+++ python-support/visualize.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import triplot
+from mpl_toolkits.mplot3d import Axes3D
+
+from mesh import Mesh2d
+
+
+def plot_triangulation(tri: Mesh2d, standalone=True, *args, **kwargs):
+    figure = plt.figure() if standalone else None
+    triplot(tri.vertices[:, 0], tri.vertices[:, 1], tri.elements, *args, **kwargs)
+    if figure:
+        figure.show()
