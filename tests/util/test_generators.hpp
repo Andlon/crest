30b20f1dc123c1b66a6fa9819ed0188c7ef16196
Andreas Longva <andreas.b.longva@gmail.com>
2016-11-07 11:50:18 +0100

    Add minimal_unit_square_mesh RapidCheck generator

--- tests/util/test_generators.hpp
+++ tests/util/test_generators.hpp
@@ -4,39 +4,56 @@
 
 #include <crest/geometry/indexed_mesh.hpp>
 
-/**
- * A generator for a small almost arbitrary course mesh for the unit square,
- * constructed through successive rounds of bisection on random elements,
- * starting from a two-element triangulation.
- */
-inline auto arbitrary_coarse_unit_square_mesh(const int max_bisection_rounds = 6)
+#include <sstream>
+
+namespace crest
 {
-    typedef crest::IndexedMesh<double, int>::Vertex Vertex;
-    typedef crest::IndexedMesh<double, int>::Element Element;
 
-    const std::vector<Vertex> coarse_vertices {
-            Vertex(0.0, 0.0),
-            Vertex(1.0, 0.0),
-            Vertex(1.0, 1.0),
-            Vertex(0.0, 1.0)
-    };
 
-    const std::vector<Element> coarse_elements {
-            Element({3, 0, 1}),
-            Element({1, 2, 3})
-    };
+    namespace gen {
+        inline auto minimal_unit_square_mesh()
+        {
+            typedef crest::IndexedMesh<double, int>::Vertex Vertex;
+            typedef crest::IndexedMesh<double, int>::Element Element;
 
-    auto mesh = crest::IndexedMesh<double, int>(coarse_vertices, coarse_elements);
+            const std::vector<Vertex> coarse_vertices {
+                    Vertex(0.0, 0.0),
+                    Vertex(1.0, 0.0),
+                    Vertex(1.0, 1.0),
+                    Vertex(0.0, 1.0)
+            };
 
-    const int num_bisections = *rc::gen::inRange(0, max_bisection_rounds);
+            const std::vector<Element> coarse_elements {
+                    Element({3, 0, 1}),
+                    Element({1, 2, 3})
+            };
+            auto mesh = crest::IndexedMesh<double, int>(coarse_vertices, coarse_elements);
+            return rc::gen::just(mesh).as("initial unit square mesh");
+        }
 
-    for (int i = 0; i < num_bisections; ++i)
-    {
-        const auto marked = *rc::gen::container<std::vector<int>>(rc::gen::inRange(0, mesh.num_elements()));
-        mesh.bisect_marked(marked);
+        /**
+         * A generator for a small almost arbitrary course mesh for the unit square,
+         * constructed through successive rounds of bisection on random elements,
+         * starting from a two-element triangulation.
+         */
+        inline auto arbitrary_unit_square_mesh(const int max_bisection_rounds = 6)
+        {
+            auto mesh = *minimal_unit_square_mesh();
+
+            const int num_bisections = *rc::gen::inRange(0, max_bisection_rounds + 1).as("number of bisection rounds");
+
+            for (int i = 0; i < num_bisections; ++i)
+            {
+                std::ostringstream ss;
+                ss << "marked elements in round " << i;
+                const auto marked = *rc::gen::container<std::vector<int>>(rc::gen::inRange(0, mesh.num_elements()))
+                    .as(ss.str());
+                mesh.bisect_marked(marked);
+            }
+
+            mesh.reset_ancestry();
+
+            return rc::gen::just(mesh).as("unit square mesh");
+        };
     }
-
-    mesh.reset_ancestry();
-
-    return rc::gen::just(mesh);
-};
+}
