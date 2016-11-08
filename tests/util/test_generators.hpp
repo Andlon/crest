cefc5e1363fff46ddadf5bdedff552cecd20745e
Andreas Longva <andreas.b.longva@gmail.com>
2016-11-08 15:22:32 +0100

    Implement arbitrary_refinement and refactor generators to leverage this

--- tests/util/test_generators.hpp
+++ tests/util/test_generators.hpp
@@ -31,29 +31,44 @@ namespace crest
             return rc::gen::just(mesh).as("initial unit square mesh");
         }
 
-        /**
-         * A generator for a small almost arbitrary course mesh for the unit square,
-         * constructed through successive rounds of bisection on random elements,
-         * starting from a two-element triangulation.
-         */
-        inline auto arbitrary_unit_square_mesh(const int max_bisection_rounds = 6)
+        inline auto arbitrary_refinement(crest::IndexedMesh<double, int> mesh,
+                                         const int min_bisection_rounds = 1,
+                                         const int max_bisection_rounds = 5)
         {
-            auto mesh = *minimal_unit_square_mesh();
-
-            const int num_bisections = *rc::gen::inRange(0, max_bisection_rounds + 1).as("number of bisection rounds");
+            const int num_bisections = *rc::gen::inRange(min_bisection_rounds,
+                                                         max_bisection_rounds + 1).as("number of bisection rounds");
 
             for (int i = 0; i < num_bisections; ++i)
             {
                 std::ostringstream ss;
                 ss << "marked elements in round " << i;
-                const auto marked = *rc::gen::container<std::vector<int>>(rc::gen::inRange(0, mesh.num_elements()))
-                    .as(ss.str());
+                // Ideally we'd just use the generator std::set here, but it seems to generate some very strange
+                // behavior that makes no sense to me
+                const auto marked = *rc::gen::map(rc::gen::container<std::vector<int>>(rc::gen::inRange(0, mesh.num_elements())),
+                                                  [] (auto vec)
+                                                  {
+                                                      return algo::sorted_unique(vec);
+                                                  })
+                        .as(ss.str());
                 mesh.bisect_marked(marked);
             }
 
-            mesh.reset_ancestry();
+            return rc::gen::just(mesh).as("refined mesh");
+        }
 
-            return rc::gen::just(mesh).as("unit square mesh");
+        /**
+         * A generator for a small almost arbitrary course mesh for the unit square,
+         * constructed through successive rounds of bisection on random elements,
+         * starting from a two-element triangulation.
+         */
+        inline auto arbitrary_unit_square_mesh(const int max_bisection_rounds = 5)
+        {
+            auto mesh_gen = arbitrary_refinement(*minimal_unit_square_mesh(), 0, max_bisection_rounds);
+
+            return rc::gen::map(mesh_gen, [] (auto mesh) {
+                mesh.reset_ancestry();
+                return mesh;
+            }).as("unit square mesh");
         };
     }
 }
